744aae9de3e7f604952537f632529ff7a9703dde
Slava Monich <slava@monich.com>
2019-07-21 14:52:01 +0300

    [test] Housekeeping

--- test/libfoil/test_cipher_aes/test_cipher_aes.c
+++ test/libfoil/test_cipher_aes/test_cipher_aes.c
@@ -5,12 +5,15 @@
  * modification, are permitted provided that the following conditions
  * are met:
  *
- *   1.Redistributions of source code must retain the above copyright
+ *  1. Redistributions of source code must retain the above copyright
  *     notice, this list of conditions and the following disclaimer.
- *   2.Redistributions in binary form must reproduce the above copyright
+ *  2. Redistributions in binary form must reproduce the above copyright
  *     notice, this list of conditions and the following disclaimer
  *     in the documentation and/or other materials provided with the
  *     distribution.
+ *  3. Neither the names of the copyright holders nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -52,7 +55,7 @@ test_padding(
     gsize data_size,
     gsize block_size)
 {
-    memset(block + data_size, 0, sizeof(block_size - data_size));
+    memset(block + data_size, 0, block_size - data_size);
 }
 
 static
