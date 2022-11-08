3f52cfc0d95cb3afb1daeec2c89f5b5adacd2d7b
Slava Monich <slava@monich.com>
2022-11-08 05:27:11 +0200

    [libfoil] Guard foil_log declaration with extern "C"

--- libfoil/include/foil_log.h
+++ libfoil/include/foil_log.h
@@ -1,16 +1,19 @@
 /*
- * Copyright (C) 2016-2019 by Slava Monich
+ * Copyright (C) 2016-2022 by Slava Monich <slava@monich.com>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
  *
- *   1.Redistributions of source code must retain the above copyright
+ *  1. Redistributions of source code must retain the above copyright
  *     notice, this list of conditions and the following disclaimer.
- *   2.Redistributions in binary form must reproduce the above copyright
+ *  2. Redistributions in binary form must reproduce the above copyright
  *     notice, this list of conditions and the following disclaimer
  *     in the documentation and/or other materials provided with the
  *     distribution.
+ *  3. Neither the names of the copyright holders nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -44,7 +47,11 @@
     log(foil_log_output) \
     log(foil_log_random)
 
+G_BEGIN_DECLS
+
 FOIL_LOG_MODULES(GLOG_MODULE_DECL)
+
+G_END_DECLS
 
 #endif /* FOIL_LOG_H */
 
